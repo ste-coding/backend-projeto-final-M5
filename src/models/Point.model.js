e61624631ed0bf04cf31e3822705628342628850
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-10-06 14:43:56 -0300

    feat: create event, favorite, point and registration models

---
+++ src/models/Point.model.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import { database } from '../config/connection.js';
+
+export const Point = database.define('Point', {
+    point_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    location: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    waste_type: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    operating_hours: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.ENUM('approved', 'pending', 'rejected'),
+        allowNull: false,
+    },
+    created_by: {
+        type: DataTypes.UUID, // FK
+        allowNull: false,
+    },
+}, {
+    tableName: 'Point',
+    timestamps: true,
+});
+
