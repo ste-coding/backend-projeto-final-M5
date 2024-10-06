bb016a5c1c4d3fcde4d32002acac5e7e1d14dfea
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-10-06 12:59:33 -0300

    feat(database): configure database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import { database } from './config/connection.js'; // Certifique-se de que o caminho está correto
 
 const app = express();
-const port = 4000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, async () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    try {
+        await database.authenticate();
+        console.log('Conexão com banco de dados estabelecida com sucesso');
+    } catch (error) {
+        console.error('Erro ao conectar com o banco de dados:', error);
+    }
 });
