0374ed48b358cdc1a47d67e10823a17f8175ac7d
unknown <raissarlsantos@gmail.com>
2024-10-13 16:05:16 -0300

    change : database

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ export default {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 5432,
   },
   test: {
     username: process.env.DB_USER,
@@ -15,7 +15,7 @@ export default {
     database: `${process.env.DB_NAME}_test`,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 5432,
   },
   production: {
     username: process.env.DB_USER,
@@ -23,6 +23,6 @@ export default {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 5432,
   },
 };
